31444a6fc0fac8d095ae95a8ed8b457ccf01a2e8
Nathan Herald <me@nathanherald.com>
2020-01-09 15:24:02 +0100

    Add standard, yarn lint is now a thing

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ export const json = (() => {
     throw new Error('cannot find a now.json file, please refer to https://github.com/myobie/deploy-now#README')
   }
 
-  let json = JSON.parse(content)
+  const json = JSON.parse(content)
 
   if (!json.name) {
     throw new Error("missing key 'name' in now.json – please include the project name in now.json and retry")
